c98ecf0141f44ce72b97c664c3c49902669b626e
Hieu Pham <35533395+phamhi@users.noreply.github.com>
2026-09-17 11:18:50 -0400

    feat(compute): add Get-AzVmInventory script on dev branch

---
+++ Compute/Get-AzVmInventory.ps1
@@ -0,0 +1,12 @@
+﻿<#
+.SYNOPSIS
+    Retrieves Azure VM inventory with power state and sizing info.
+#>
+function Get-AzVmInventory {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $false)]
+        [string]$ResourceGroupName
+    )
+    Write-Output "Querying Azure Virtual Machines inventory on dev branch..."
+}
